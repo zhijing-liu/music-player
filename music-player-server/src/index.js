807324595dc9ba2a25f71bdcd63bb93ff316582d
纸境 <18635149303@163.com>
2022-12-03 02:49:07 +0800

    gzip 优化

--- music-player-server/src/index.js
+++ music-player-server/src/index.js
@@ -4,10 +4,11 @@ import {join} from 'node:path'
 import {fileURLToPath} from 'node:url'
 import {apiRouter} from "./router.js";
 import {port, host} from "./config.js";
-
+import compression from 'compression'
 const path = join(fileURLToPath(import.meta.url), '../../')
 const publicPath=join(path,'/public')
 const app = express()
+app.use(compression({filter:  (req, res) =>req.headers['accept-encoding']?.includes('gzip')&&compression.filter(req, res)}))
 app.use('/api', apiRouter)
 app.get('/musicStatic/:musicName', (req, res) => {
     res.send(readFileSync(join(publicPath, './MyMusic', req.params.musicName)))
@@ -28,6 +29,6 @@ app.get('/musicPlayer', (req, res) => {
 app.listen(port, host, () => {
     readFile(join(publicPath, './logo.txt'), {encoding: 'utf-8'}, (err, data) => {
         console.clear()
-        console.log(data.replaceAll('{{link}}', `http://${host}${port === 80 ? '' : `:${port}`}/musicPlayer`))
+        console.log(data.replaceAll('{{link}}', `http://${(host===true||host==='0.0.0.0')?'127.0.0.1':host}${port === 80 ? '' : `:${port}`}/musicPlayer`))
     })
 })
